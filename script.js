8df61af1b68c1bfa4d4e892aa13bc67bc1f997d6
minnukota381 <minnukota381@gmail.com>
2024-07-05 08:26:24 +0530

    feat: "Added go-to-top button, updated CSS for icons, animations, hover effects, and scrollbar; removed unnecessary line breaks in HTML"

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
-    $('a[href^="#"]').on('click', function(e) {
-        e.preventDefault();
-        let target = this.hash;
-        $('html, body').animate({
-            scrollTop: $(target).offset().top
-        }, 500);
-    });
+  $('a[href^="#"]').on('click', function(e) {
+      e.preventDefault();
+      let target = this.hash;
+      let $target = $(target);
+      
+      if ($target.length) {
+          $('html, body').animate({
+              scrollTop: $target.offset().top
+          }, 500);
+      }
+  });
 });
 
 window.addEventListener('scroll', reveal);
@@ -14,16 +18,31 @@ function reveal() {
   let reveals = document.querySelectorAll('.reveal');
   
   reveals.forEach(function(reveal){
-    let windowHeight = window.innerHeight;
-    let revealTop = reveal.getBoundingClientRect().top;
-    let revealPoint = 150;
-    
-    if(revealTop < windowHeight - revealPoint){
-      reveal.style.opacity = 1;
-      reveal.style.transform = 'translateY(0)';
-    } else {
-      reveal.style.opacity = 0;
-      reveal.style.transform = 'translateY(-20px)';
-    }
+      let windowHeight = window.innerHeight;
+      let revealTop = reveal.getBoundingClientRect().top;
+      let revealPoint = 150;
+      
+      if (revealTop < windowHeight - revealPoint) {
+          reveal.style.opacity = 1;
+          reveal.style.transform = 'translateY(0)';
+      } else {
+          reveal.style.opacity = 0;
+          reveal.style.transform = 'translateY(-20px)';
+      }
   });
 }
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+          $('#goTopButton').fadeIn();
+      } else {
+          $('#goTopButton').fadeOut();
+      }
+  });
+
+  $('#goTopButton').click(function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+  });
+});
